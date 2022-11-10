6dd44ab80618be56fb259717ea10a6b602136928
Bryam Jesus Talledo Garcia <bryamtalledog@gmail.com>
2022-11-09 21:43:06 -0500

    Update Pedido Page

--- src/pages/PedidoPage.js
+++ src/pages/PedidoPage.js
@@ -31,22 +31,37 @@ function PedidoPage() {
     setListaPedidos(result.data);
   };
 
+  const listarPlatos = async () => {
+    const result = await listarPlatoService();
+    setListaPlatos(result.data);
+  };
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    const nDatos = { ...datos, [name]: value };
+    setDatos(nDatos);
+  };
+
   const handleShowModal = () => setShowModal(true);
+
   const handleCloseModal = () => {
+    setTituloModal("Nuevo Pedido");
     setShowModal(false);
   };
 
   const handleShowData = (id) => {
+    setTituloModal("Editar Pedido");
     handleShowModal();
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    handleCloseModal()
+    handleCloseModal();
   };
 
   useEffect(() => {
     listarPedidos();
+    listarPlatos();
   }, []);
 
   return (
@@ -90,12 +105,65 @@ function PedidoPage() {
         </tbody>
       </table>
 
-      <Modal show={showModal} onHide={handleCloseModal}>
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+      >
         <form onSubmit={handleSubmit}>
           <Modal.Header closeButton>
             <Modal.Title>{tituloModal}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            <div className="row">
+              <div className="col-md-8">
+                <div className="input-group">
+                  <select ref={platoRef} className="form-select">
+                    <option value="">Seleccionar un plato</option>
+                    {listaPlatos.map((plato) => (
+                      <option key={plato._id} value={platoRef._id}>
+                        {plato.nombre} = S/{plato.precio}
+                      </option>
+                    ))}
+                  </select>
+                  <Button type="button" variant="secondary">
+                    Agregar
+                  </Button>
+                </div>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>Plato</th>
+                      <th>Precio</th>
+                      <th>Estado</th>
+                      <th>Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {datos.detalle.map((item, index) => (
+                      <tr key={index}>
+                        <td>{item.plato}</td>
+                        <td>{item.precio}</td>
+                        <td>{item.estado}</td>
+                        <td>
+                          <Button variant="danger" size="sm">
+                            <i className="bi bi-trash"></i>
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-md-4">
+                <h4 className="d-flex justify-content-between">
+                  Total <span>S/ {datos.total}</span>
+                </h4>
+              </div>
+            </div>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleCloseModal}>
               Cancelar
